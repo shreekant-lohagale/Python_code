ab40ce77924ff893f36ab70d8acd816aeb33c13a
user.email "shreekantlohagale@gmail.com <git config --global user.email "shreekantlohagale@gmail.com>
2024-11-11 22:18:25 +0530

    second-commit

---
+++ pra8.py
@@ -0,0 +1,6 @@
+# Import the sum_of_numbers function from the math_operations package
+from pra8_2 import sum_of_numbers
+
+# Call the function
+result = sum_of_numbers(10, 20)
+print(f"The sum of the numbers is: {result}")
